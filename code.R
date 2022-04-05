7b74e5a099f35240093c7b830a561c71025281f8
stefano marzorati <marzoste3@gmail.com>
2022-04-05 15:35:19 +0200

    Xts e Missing Value

--- code.R
+++ code.R
@@ -1,15 +1,25 @@
 library(readr)
-library(lubridate)
+library(xts)
+library(imputeTS) #per vedere info sui missing values
 
 dataset <- read_csv("Project_data_2021_2022 (TRAINSET).csv", col_types = cols(Date = col_character()))
-dataset$Hour <- paste(dataset$Hour, ":00:00", sep="")
 
-dataset$dttm <- ymd_hms(paste(dataset$Date, dataset$Hour, sep = " "))
+data <- dataset$CO
+dates <- seq(as.POSIXct("2004-03-10 18:00:00"), as.POSIXct("2005-02-28 23:00:00"), by="hour")
+y <- xts(x=data, order.by=dates)
+
+plot(y)
+
+## INFORMAZIONI MISSING VALUES
+
+ggplot_na_distribution(y)
+ggplot_na_intervals(tsNH4)
+statsNA(y)
+y_imp <- na_kalman(y)
 
 
-firstHour <- 24*(as.Date("2004-03-10 18:00:00")-as.Date("2004-1-1 00:00:00"))
-tt <- ts(dataset$CO,start=c(2006,firstHour),frequency=24*365)
+#PER ARIMA
+# stazionarietà in varianza
+# stazionarietà in media e quindi anche togliere la stagionalità
+# studio gli acf e pacf
 
-plot(dataset$CO, type = 'l')
-
-plot(tt)
