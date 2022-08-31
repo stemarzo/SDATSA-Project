bc0b4c3afd15ebb256e4fc4546fb4fd7012319b1
stefano marzorati <marzoste3@gmail.com>
2022-08-31 18:47:19 +0200

    ucm

--- code.R
+++ code.R
@@ -23,7 +23,7 @@ par(mfrow=c(1,1))
 train_date <- nrow(y_imp) *0.8
 y_train <- y_imp[1:train_date,]
 y_test <- ts(y_imp[-c(1:train_date),], frequency = 24)
-plot(y_train)
+plot(log(y_train))
 
 write.csv(y_train, file = "y_train.csv", row.names = FALSE)
 write.csv(y_test, file = "y_test.csv", row.names = FALSE)
@@ -79,7 +79,7 @@ ss <- sin(omega[,-84])
 
 
 mod2 <- Arima(y_train, c(1, 0, 1),
-              list(order = c(3, 1, 0), period = 24),
+              list(order = c(3, 1, 1), period = 24),
               include.drift = TRUE,
               xreg = cbind(cc[,1:3], ss[, 1:3])[1:length(y_train), ])
 summary(mod2)
@@ -95,6 +95,272 @@ plot(fcst2, 0)
 mean(abs((y_test - as.numeric(fcst2$mean))/y_test)) * 100
 
 
+# UCM
+
+library(KFAS)
+
+y_train = ts(y_train, frequency = 24)
+
+a1_in = c(y_train[1],0)
+model = SSModel(y_train ~ SSMtrend(2,Q = list(0,NA), a1 = a1_in) +
+                  SSMseasonal(24, sea.type = "dummy", Q = NA, P1inf = diag(23)), 
+                              H= NA)
+updt = function(pars, model){
+  model$Q[1,1,1]= exp(pars[1])#varianza level
+  model$Q[2,2,1]= exp(pars[2])#slope
+  model$Q[3,3,1]= exp(pars[3])#stag
+  model$H[1,1,1]= exp(pars[4])#errore di y
+  model
+}
+
+updt_irw = function(pars, model){
+  model$Q[2,2,1]= exp(pars[1])#slope
+  model$Q[3,3,1]= exp(pars[2])#stag
+  model$H[1,1,1]= exp(pars[3])#errore di y
+  model
+}
+
+
+vy = var(diff(y_train)) 
+init = rep(log(vy/100), 4)
+init_irw = c(log(vy/10000), log(vy/100),log(vy/100))
+
+# fit = fitSSM(model, init, updatefn = updt, method="BFGS")
+fit = fitSSM(model, init_irw, updatefn = updt_irw, method="BFGS")
+
+fit$optim.out$convergence #se è zero la stima ha raggiunto convergenza
+
+smo = KFS(fit$model, filtering = "state", smoothing = "state")
+
+smo$alphahat
+
+dim(smo$alphahat)
+colnames(smo$alphahat)
+level = smo$alphahat[,"level"]
+plot(level)
+
+seas = smo$alphahat[, 3]
+plot(seas[1:168], type = "l")
+
+pred = ts(predict(fit$model, n.ahead = 1706), frequency = 24)
+plot(pred, type ="l")
+y_test = ts(y_test, frequency = 24)
+lines(y_test, col=2)
+
+mean(abs((y_test - as.numeric(pred))/y_test)) * 100
+smo$alphahat[6820,'slope']
+
+time = 1:length(y_train)
+time2 = time*time
+
+model = SSModel(ts(y_train, frequency = 24) ~ SSMregression( ~ time) +
+                  SSMseasonal(24, sea.type = "dummy", Q = NA, P1inf = diag(23)), 
+                H= NA)
+
+updt_tr2 = function(pars, model){
+  model$Q[1,1,1]= exp(pars[1])#stag
+  model$H[1,1,1]= exp(pars[2])#errore di y
+  model
+}
+
+init_trend = init_irw[-1]
+
+fit = fitSSM(model, init_trend, updatefn = updt_tr2, method="BFGS")
+
+fit$optim.out$convergence #se è zero la stima ha raggiunto convergenza
+
+smo = KFS(fit$model, filtering = "state", smoothing = "state")
+
+smo$alphahat
+
+dim(smo$alphahat)
+colnames(smo$alphahat)
+level = smo$alphahat[,"(Intercept)"] + smo$alphahat[,"time"]*time #+
+  #smo$alphahat[,"time2"]*time2
+plot(level, ylim=c(800,2000))
+
+seas = smo$alphahat[, 3]
+plot(seas[1:168], type = "l")
+
+plot(level[1:336]+seas[1:336], type = 'l')
+
+time.test = (8526-1705):8526
+time.test[1]
+y.new = y_test
+y.new[1:nrow(y.new),1:ncol(y.new)] = NA
+y.new = ts(y.new, frequency = 24)
+# mZ deve essere un array 1x26x1706
+# i primi 10 valori sulla diagonale devono essere il prezzo della benzina e l'ind_fid_cons (alternati)
+mZ = array(data = fit$model$Z[,,1:1706], dim = c(1,25,1706))
+head(fit$model$Z)
+mZ[1,2,1:1706] = time.test
+#mZ[1,3,1:1706] = time.test^2
+mT = fit$model$T
+mR = fit$model$R
+mQ = fit$model$Q
+va1 = fit$model$a1 
+mP1 = fit$model$P1
+mP1i = fit$model$P1inf
+mH = fit$model$H
+statename = row.names(fit$model$P1)
+
+newdata = SSModel(y.new ~ -1 + SSMcustom(Z = mZ, T = mT, R = mR, Q = mQ,
+                                         a1 = va1, P1 = mP1, P1inf = mP1i,
+                                         state_names = statename), H = mH)
+
+pred = ts(predict(fit$model, newdata = newdata),frequency = 24)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pred = ts(predict(fit$model, n.ahead = 1706), frequency = 24 )
+plot(pred, type ="l")
+y_test = ts(y_test, frequency = 24)
+lines(y_test, col=2)
+
+mean(abs((y_test - as.numeric(pred))/y_test)) * 100
+
+
+
+
+
+
+
+
+
+smo$alphahat
+
+model = SSModel(ts(y_train, frequency = 24) ~ SSMcycle(period = 8760, Q = NA) +
+                  SSMseasonal(168, sea.type = "dummy", Q = NA, P1inf = diag(167)), 
+                H= NA)
+model$Q
+
+updt_cycle = function(pars, model){
+  model$Q[1,1,1] = model$Q[2,2,1]= exp(pars[1])#ciclo
+  model$Q[3,3,1]= exp(pars[2])#stag
+  model$H[1,1,1]= exp(pars[3])#errore di y
+  model
+}
+
+init_cycle = c(log(vy/10),log(vy/10),log(vy/10))
+
+fit = fitSSM(model, init_cycle, updatefn = updt_cycle, method="BFGS")
+fit$optim.out$convergence #se è zero la stima ha raggiunto convergenza
+smo = KFS(fit$model, filtering = "state", smoothing = "state")
+
+dim(smo$alphahat)
+colnames(smo$alphahat)
+level = smo$alphahat[,"(Intercept)"] + smo$alphahat[,"cycle"]
+plot(level, ylim=c(800,2000))
+
+seas = smo$alphahat[, 2]
+plot(seas[1:168], type = "l")
+
+plot(level[1:336]+seas[1:336], type = 'l')
+lines(as.numeric(y_train[1:336]),col = 2)
+pred = ts(predict(fit$model, n.ahead = 1706), frequency = 24 )
+plot(pred, type ="l")
+y_test = ts(y_test, frequency = 24)
+lines(y_test, col=2)
+
+mean(abs((y_test - as.numeric(pred))/y_test)) * 100
+
+
+
+
+
+
+
+
+
+
+time = 1:length(y_train)
+
+model = SSModel(ts(y_train, frequency = 24) ~ SSMregression( ~ time) +
+                  SSMcycle(period = 8760, Q = NA) +
+                  SSMseasonal(24, sea.type = "dummy", Q = NA, P1inf = diag(23)), 
+                H= NA)
+
+updt_cycle = function(pars, model){
+  model$Q[1,1,1] = model$Q[2,2,1]= exp(pars[1])#ciclo
+  model$Q[3,3,1]= exp(pars[2])#stag
+  model$H[1,1,1]= exp(pars[3])#errore di y
+  model
+}
+
+init_cycle = c(log(vy/10),log(vy/10),log(vy/10))
+
+fit = fitSSM(model, init_cycle, updatefn = updt_cycle, method="BFGS")
+
+fit$optim.out$convergence #se è zero la stima ha raggiunto convergenza
+
+smo = KFS(fit$model, filtering = "state", smoothing = "state")
+
+smo$alphahat
+
+dim(smo$alphahat)
+colnames(smo$alphahat)
+level = smo$alphahat[,"(Intercept)"] + smo$alphahat[,"time"]*time +smo$alphahat[,"cycle"]
+plot(level, ylim=c(800,2000))
+
+seas = smo$alphahat[, 3]
+plot(seas[1:168], type = "l")
+
+plot(level[1:336]+seas[1:336], type = 'l')
+
+time.test = (8526-1705):8526
+time.test[1]
+y.new = y_test
+y.new[1:nrow(y.new),1:ncol(y.new)] = NA
+y.new = ts(y.new, frequency = 24)
+# mZ deve essere un array 1x26x1706
+# i primi 10 valori sulla diagonale devono essere il prezzo della benzina e l'ind_fid_cons (alternati)
+mZ = array(data = fit$model$Z[,,1:1706], dim = c(1,27,1706))
+head(fit$model$Z)
+mZ[1,2,1:1706] = time.test
+#mZ[1,3,1:1706] = time.test^2
+mT = fit$model$T
+mR = fit$model$R
+mQ = fit$model$Q
+va1 = fit$model$a1 
+mP1 = fit$model$P1
+mP1i = fit$model$P1inf
+mH = fit$model$H
+statename = row.names(fit$model$P1)
+
+
+newdata = SSModel(y.new ~ -1 + SSMcustom(Z = mZ, T = mT, R = mR, Q = mQ,
+                                         a1 = va1, P1 = mP1, P1inf = mP1i,
+                                         state_names = statename), H = mH)
+
+pred = ts(predict(fit$model, newdata = newdata),frequency = 24)
+
+
+plot(pred, type ="l", ylim= c(800,2000))
+y_test = ts(y_test, frequency = 24)
+lines(y_test, col=2)
+
+mean(abs((y_test - as.numeric(pred))/y_test)) * 100
